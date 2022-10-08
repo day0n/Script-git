c532c2fcf523f3d8c6d339c6e851745875c987e6
day0n <niuzj0@gmail.com>
2022-10-08 22:54:51 +0800

    uptata README

--- script.sh
+++ script.sh
@@ -2,11 +2,11 @@
 declare -i number=$RANDOM+1000000000
 declare -i max=7-3+1;
 times=$(($number%$max+3));
-echo $times
+echo "执行次数：" $times
 
 for((integer = 1; integer <= $times; integer++))
 do
-    echo 'hello niu'>>README.md
+    echo 'hello niu'>README.md
     git add README.md
     git commit -m "uptata README"
     git push
